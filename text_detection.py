95929d08c3b78afdbaa86c2b150c09129f8e5a8c
Jose Luis Muñoz <42367982+ioseluiz@users.noreply.github.com>
2024-06-14 14:57:21 -0500

    Headers OK

--- text_detection.py
+++ text_detection.py
@@ -3,6 +3,33 @@ import re
 import csv
 from pathlib import Path
 from paddleocr import PaddleOCR
+import multiprocessing as mp
+
+root_folder = os.getcwd()
+# imgs_folder = Path(root_folder) / "imgs"
+imgs_folder = r"C:\Users\jlmunoz\OneDrive - Autoridad del Canal de Panama\Documents\tasks\2024\expats\imgs"
+ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
+
+
+def divide_list(original_list, n):
+    sublists = []
+    # Create empty sublists
+    for i in range(0,n):
+        sublists.append({})
+    # Add elements to each sublist
+    max_index = int(len(original_list) / n)
+    for i in range(0, n):
+        if i == n-1:
+            sublists[i] = {"index": i, "data": original_list[(i*max_index):]}
+        else:
+            sublists[i] = {"index": i, "data": original_list[(i*max_index):max_index*(i+1)]}
+
+    total = 0
+    for i in range(0,n):
+        print(len(sublists[i]['data']))
+        total += len(sublists[i]['data'])
+    print(total)
+    return sublists
 
 def save_csv(filename: str, headers: list[str], data: list[dict]) -> None:
     with open(filename, 'w', encoding="utf-8",newline='') as csv_file:
@@ -10,6 +37,7 @@ def save_csv(filename: str, headers: list[str], data: list[dict]) -> None:
         writer.writeheader()
         writer.writerows(data)
         print(f"CSV file {filename} was generated successfully...")
+
 
 def print_data(data: list[dict]) -> None:
     for item in data:
@@ -20,76 +48,112 @@ def get_page(text):
     result = pattern.findall(text)
     return int(result[0][5:])
 
-def get_month(text):
+def validate_month(text):
     pass
 
-def get_year(text):
+def validate_year(text: str) -> str:
     pattern = re.compile(r"[0-9]{4}")
-    result = pattern.findall(text)
-    return int(result[0])
+    if re.fullmatch(pattern, text):
+        return text
+    else:
+        return ""
 
 def get_name(text):
     pass
 
-def get_badge(text):
-    pass
+def validate_badge(text: str) -> str:
+    pattern = re.compile(r"[0-9]{6}")
+    if re.fullmatch(pattern, text):
+        return text
+    else:
+        return ""
 
 def get_department(text):
     pass
-    
 
-def main():
-    ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
+def header_extraction(imgs_files: list[str], ind: int):
     data = []
     files_errors = []
-    headers = ["file", "page","name","badge", "month","year","department"]
-    root_folder = os.getcwd()
-    imgs_folder = Path(root_folder) / "imgs"
-    # Get contents of imgs folder
-    imgs_files = os.listdir(imgs_folder)
-    counter = 1 # for testing
+    error_headers = ["filename"]
+    headers = ["id","file", "page","name","badge", "month","year","department"]
+    counter = 1
     for img in imgs_files:
         words = []
         name = ""
         department = ""
-        if counter < 1001:
-            full_img_path = Path(imgs_folder) / img
-            print(f"No. {counter} --- File: {full_img_path}")
-            try:
-                result = ocr.ocr(str(full_img_path))
-                inner_result = result[0]
-                # print(len(inner_result))
-                for i in range(0,20):
-                    if "MES" in inner_result[i][1][0]:
-                        month = inner_result[i+1][1][0]
-                        year = inner_result[i+2][1][0]
-                    if  inner_result[i][1][0] == "LAST AND FIRST NAME":
-                        name = inner_result[i+1][1][0]
-                    if "BADGE" in inner_result[i][1][0]:
-                        badge = inner_result[i+1][1][0]
-                    if "DEPARTMENT" in inner_result[i][1][0]:
-                        department = inner_result[i+1][1][0]
-                    # print(inner_result[i][1][0])
-            
-                info = {
-                    "file": img,
-                    "page": get_page(img),
-                    "name": name,
-                    "badge": badge,
-                    "month": month,
-                    "year": year,
-                    "department": department,
-                    
-                }
-                data.append(info)
-            except:
-                files_errors.append(img)
+        # if (counter >= 1) and (counter < 2000):
+        full_img_path = Path(imgs_folder) / img
+        print(f"Process: {ind} --- No. {counter} --- File: {full_img_path}")
+        try:
+            result = ocr.ocr(str(full_img_path))
+            inner_result = result[0]
+            # print(len(inner_result))
+            for i in range(0,20):
+                if "MES" in inner_result[i][1][0]:
+                    month = inner_result[i+1][1][0]
+                    year = inner_result[i+2][1][0]
+                    year = validate_year(year)
+                if  inner_result[i][1][0] == "LAST AND FIRST NAME":
+                    name = inner_result[i+1][1][0]
+                if "BADGE" in inner_result[i][1][0]:
+                    badge = inner_result[i+1][1][0]
+                    badge = validate_badge(badge)
+                if "DEPARTMENT" in inner_result[i][1][0]:
+                    department = inner_result[i+1][1][0]
+                # print(inner_result[i][1][0])
+        
+            info = {
+                "id": counter,
+                "file": img,
+                "page": get_page(img),
+                "name": name,
+                "badge": badge,
+                "month": month,
+                "year": year,
+                "department": department,
                 
-            counter += 1
+            }
+            data.append(info)
+        except:
+            files_errors.append({"filename": img})
+                
+        counter += 1
         
     # print_data(data)
     print_data(files_errors)
-    save_csv("extracted_data.csv", headers, data)
+    root_folder = os.getcwd()
+    errors_folder = Path(root_folder) / "errors"
+    test_data_folder = Path(root_folder) / "test_data"
+    save_csv(f"{test_data_folder}/{ind}_extracted_data_test.csv", headers, data)
+    save_csv(f"{errors_folder}/{ind}_errors_files.txt", error_headers, files_errors)
+    
+
+def main():
+    
+    
+    
+    # Get contents of imgs folder
+    imgs_files = os.listdir(imgs_folder)
+    # Limit the quantity of files
+    imgs_files = imgs_files[8001:10001]
+    # for img in imgs_files:
+    #     print(img)
+     # for testing
+
+    list_imgs_divided = divide_list(imgs_files, 14)
+    folders = list_imgs_divided
+
+
+    procs = []
+    for item in folders:
+        proc = mp.Process(target=header_extraction, args=(item['data'],item['index']))
+        procs.append(proc)
+        proc.start()
+
+    for proc in procs:
+        proc.join()
+
+    
     
     
     
